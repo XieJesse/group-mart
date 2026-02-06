ba0996eceb46f349bf1077df7efc4389045536f6
JesseStuy20 <jxie20@stuy.edu>
2026-02-05 22:31:37 -0500

    updates

--- orders.js
+++ orders.js
@@ -7,16 +7,31 @@ btn.className = "w_hhLG w_XK4d w_jDfj" ;
 btn.style.marginLeft = "20px" ;
 btn.style.width = "185px" ;
 btn.addEventListener('click',(e)=>{
-  const cartTotal = document.getElementsByClassName("db tr b f5 pb3")[0].innerHTML.substring(1) ;
+  var cartT = document.getElementsByClassName("db tr b f5 pb3")[0] ;
+  // console.log(cartT) ;
+  var cartTotal ;
+
+  if (cartT == undefined) {
+    cartT = document.getElementsByClassName("w_U9_0 w_U0S3 w_QcqU b")[0] ;
+    var cartTotal = cartT.innerText.substring(1) ;
+    // console.log(cartT) ;
+  }
+  else {
+    var cartTotal = cartT.innerHTML.substring(1) ;
+  }
+  
   // console.log("cart total " + cartTotal) ;
   const cartTax = document.getElementsByClassName("w_U9_0 w_U0S3 w_QcqU")[1].innerHTML.substring(1) ;
   // console.log(cartTax) ;
   const cartTip = document.getElementsByClassName("w_U9_0 w_U0S3 w_QcqU")[3].innerHTML.substring(1) ;
   // console.log(cartTip) ;
-  const matches = document.querySelectorAll('[data-testid=category-accordion-Shopped]')[0].getElementsByClassName("pa3 pb0 ph4-m") ;
+  const matchesOne = document.querySelectorAll('[data-testid=category-accordion-Shopped]')[0].getElementsByClassName("pa3 pb0 ph4-m") ;
+  const matchesTwo = document.querySelectorAll('[data-testid=category-accordion-Weight-adjusted]')[0].getElementsByClassName("pa3 pb0 ph4-m") ;
+
   // console.log(document.querySelectorAll('[data-testid=category-accordion-Shopped]')) ;
   // "w_hhLG w_DZvO w_0_LY dib h-auto mv3 no-underline"
-  for (var match of matches) {
+  console.log(matchesTwo)
+  for (var match of matchesOne) {
     // console.log(match) ;
     var name = match.getElementsByClassName("w_V_DM")[0].innerHTML ;
     // console.log(name) ;
@@ -29,6 +44,20 @@ btn.addEventListener('click',(e)=>{
     // console.log(cost) ;
     cart[name] = [count,cost] ;
   }
+  for (var match of matchesTwo) {
+    // console.log(match) ;
+    var name = match.getElementsByClassName("w_V_DM")[0].innerHTML ;
+    // console.log(name) ;
+    var count = match.getElementsByClassName("pt1 f7 f6-m bill-item-quantity gray")[0].innerHTML.substring(4) ;
+    // console.log(count) ;
+    if (match.getElementsByClassName("w_iUH7")[0] != undefined) {
+      match.getElementsByClassName("w_iUH7")[0].remove() ;
+    }
+    var cost = match.getElementsByClassName("f5 b black tr")[0].innerText.substring(1) ;
+    // console.log(cost) ;
+    cart[name] = [count,cost] ;
+  }
+
   // chrome.storage.local.get(null, function(items) {
   //   var allKeys = Object.keys(items);
   //   console.log(allKeys);
